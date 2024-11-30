e471da16c832b45dd4a54123c2738eb5612b2ba8
Shoaib Tamboli <shoaibtamboli03@gmail.com>
2024-12-01 00:15:26 +0530

    added given tasks

--- BD-5/bd5.1-HW1/index.js
+++ BD-5/bd5.1-HW1/index.js
@@ -51,3 +51,38 @@ app.get('/seed_db', async (req, res) => {
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
+
+
+/* 
+Sequelize Installation & Setup
+
+Install the following packages from the Dependencies tab in Replit
+
+express, sequelize , sqlite3
+
+Create a folder named lib and create a file inside this folder named index.js
+
+Create a sequelize instance
+
+Export both sequelize instance and DataTypes which will be used to create models in the next step
+
+Creating Models
+
+Create a folder named models and create a file inside this folder named post.model.js
+
+Import the sequelize instance & DataTypes from the /lib/index.js file
+
+Define a model named post with the columns name as TEXT, author as TEXT, title as TEXT, content as TEXT
+
+Export the model from the file
+
+Seeding the database with data
+
+Create an express server with an endpoint /seed_db which will seed the database with dummy data
+
+Declare a variable named posts which will contain an array of objects with dummy posts.
+
+Populate the variable posts with dummy post-data.
+
+Use the model post and bulkCreate method to seed dummy data into the database file whenever the user visits the /seed_db endpoint.
+*/
--- BD-5/bd5.1-HW3/index.js
+++ BD-5/bd5.1-HW3/index.js
@@ -51,3 +51,39 @@ app.get('/seed_db', async (req, res) => {
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
+
+/*
+Make your own array of dummy data. The datatype of this array of objects should match that of the model. You can refer the dummy data of BD5.1_CW for examples
+
+Sequelize Installation & Setup
+
+Install the following packages from the Dependencies tab in Replit
+
+express, sequelize , sqlite3
+
+Create a folder named lib and create a file inside this folder named index.js
+
+Create a sequelize instance
+
+Export both sequelize instance and DataTypes which will be used to create models in the next step
+
+Creating Models
+
+Create a folder named models and create a file inside this folder named book.model.js
+
+Import the sequelize instance & DataTypes from the /lib/index.js file
+
+Define a model named book with the columns title as TEXT, author as TEXT, description as TEXT, genre as TEXT
+
+Export the model from the file
+
+Seeding the database with data
+
+Create an express server with an endpoint /seed_db which will seed the database with dummy data
+
+Declare a variable named books which will contain an array of objects with dummy books.
+
+Populate the variable books with dummy books data
+
+Use the model books and bulkCreate method to seed dummy data into the database file whenever the user visits the /seed_db endpoint.
+*/

24cb08a6e0345c9ed81731074d18377c484d13c3
Shoaib Tamboli <shoaibtamboli03@gmail.com>
2025-01-01 16:39:02 +0530

    complted 5.3 HW

--- BD-5/bd-5.3-CW-POST/index.js
+++ BD-5/bd-5.3-CW-POST/index.js
@@ -6,6 +6,7 @@ const port = 3000;
 let { track } = require('./models/track.model.js');
 let { sequelize } = require('./lib/index.js');
 
+app.use(express.json());
 let trackData = [
   {
     name: 'Raabta',
@@ -89,6 +90,18 @@ let trackData = [
   },
 ];
 
+app.get('/seed_db', async (req, res) => {
+  try {
+    await sequelize.sync({ force: true });
+    await track.bulkCreate(trackData);
+    res.status(200).json({ message: 'Database seeding successful' });
+  } catch (err) {
+    res
+      .status(500)
+      .json({ error: 'Error saving the data, Error: ' + err.message });
+  }
+});
+
 /*
 Exercise 1: Fetch all tracks
 
@@ -100,6 +113,23 @@ API Call
 
 http://localhost:3000/tracks
 */
+
+async function fetchAllTracks() {
+  const tracks = await track.findAll();
+  return { tracks };
+}
+
+app.get('/tracks', async (req, res) => {
+  try {
+    const data = await fetchAllTracks();
+    if (data.tracks.length === 0) {
+      return res.status(404).json({ message: `No tracks found` });
+    }
+    return res.status(200).json(data);
+  } catch (err) {
+    return res.status(500).json({ error: err.message });
+  }
+});
 
 /* 
 Exercise 2: Add a new track in the database
@@ -114,6 +144,21 @@ API Call
 
 http://localhost:3000/tracks/new
 */
+
+async function addNewTrack(newTracks) {
+  const newTrack = await track.create(newTracks);
+  return { newTrack };
+}
+
+app.post('/tracks/new', async (req, res) => {
+  try {
+    const newTrack = req.body.newTrack;
+    const response = await addNewTrack(newTrack);
+    return res.status(200).json(response);
+  } catch (err) {
+    return res.status(500).json({ error: err.message });
+  }
+});
 
 /*
 Exercise 3: Update track information
@@ -131,6 +176,30 @@ API Call
 http://localhost:3000/tracks/update/11
 */
 
+async function updateTrackById(newTrackData, id) {
+  const trackDetails = await track.findOne({ where: { id } });
+  if (!trackDetails) {
+    return {};
+  }
+  trackDetails.set(newTrackData);
+  let updatedTrackDetails = await trackDetails.save();
+  return { message: 'Track updated succesfully', updatedTrackDetails };
+}
+
+app.post('/tracks/update/:id', async (req, res) => {
+  try {
+    const id = parseInt(req.params.id);
+    const newTrackData = req.body.newTrackData;
+    const response = await updateTrackById(newTrackData, id);
+    if (!response.message) {
+      return res.status(404).json({ message: `Track not found` });
+    }
+    return res.status(200).json(response);
+  } catch (err) {
+    return res.status(500).json({ error: err.message });
+  }
+});
+
 /*
 Exercise 4: Delete a track from the database
 
@@ -144,6 +213,29 @@ API Call
 
 http://localhost:3000/tracks/delete
 */
+
+async function deleteById(id) {
+  const destoyedTrack = await track.destroy({ where: { id } });
+  if (destoyedTrack === 0) {
+    return {};
+  }
+  return { message: `Track record Deleted` };
+}
+
+app.post('/tracks/delete', async (req, res) => {
+  try {
+    const id = parseInt(req.body.id);
+    const response = await deleteById(id);
+    if (!response.message) {
+      res.status(404).josn({ message: 'Track not found' });
+    }
+
+    res.status(200).json(response);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
